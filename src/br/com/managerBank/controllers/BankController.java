cbb41867cbad125379565e070f36ad4f2a8a2f78
Leandro Sousa Silva <135087138+Eusousleandro@users.noreply.github.com>
2026-08-22 17:50:38 -0300

    Implementando Service controller do banco

---
+++ src/br/com/managerBank/controllers/BankController.java
@@ -0,0 +1,24 @@
+package br.com.managerBank.controllers;
+
+public class BankController {
+    private final static Scanner scanner = new Scanner(System.in);
+    private final static BankService bankService
+
+    public BankController(BankService bankService) {
+        this.bankService = bankService:
+    }
+
+    public static void deposit() {
+        System.out.println("What the value to you want deposite?");
+        double value = scanner.nextDouble();
+        System.out.println("The deposite in value " + value + " with sucess");
+    }
+
+    public static void withDraw() {
+
+    }
+
+    public static void showBalance() {
+
+    }
+}
