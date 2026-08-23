3ddce7cd61c5d5b02a2e6915e7d2bc4046aa8a00
Leandro Sousa Silva <135087138+Eusousleandro@users.noreply.github.com>
2026-08-23 17:06:40 -0300

    Solucionando um bug no menu principal

--- src/br/com/managerBank/Main.java
+++ src/br/com/managerBank/Main.java
@@ -7,30 +7,29 @@ import java.util.Scanner;
 public class Main {
 
     private final static Scanner scanner = new Scanner(System.in);
+    private final static BankController controller = new BankController();
 
-        public static void main(String[] agra) {
-                
-            BankController controller = new BankController();
+    public static void main(String[] agra) {
 
-            int options;
+        int options;
             
-            do {
-                System.out.println("********** Welcome, MANAGER BANK **********");
-                System.out.println("What are you going to do today?");
-                System.out.println("1- Deposit");
-                System.out.println("2- Withdraw");
-                System.out.println("3- Transfer");
-                System.out.println("4- Balance");
-                System.out.println("0- Sair");
+        do {
+            System.out.println("********** Welcome, MANAGER BANK **********");
+            System.out.println("What are you going to do today?");
+            System.out.println("1- Deposit");
+            System.out.println("2- Withdraw");
+            System.out.println("3- Transfer");
+            System.out.println("4- Balance");
+            System.out.println("0- Sair");
 
-                switch(options) {
-                        case 1 -> controller.deposit();
-                        case 2 -> controller.withdraw();
-                        case 3 -> controller.transferer();
-                        case 4 -> controller.showBalance();
-                        case 0 -> System.exit(0);
-                        default -> System.out.println("Invalid options");
-                    }
-                } while(options != 0);
-            }
+            switch(options) {
+                    case 1 -> controller.deposit();
+                    case 2 -> controller.withdraw();
+                    case 3 -> controller.transferer();
+                    case 4 -> controller.showBalance();
+                    case 0 -> System.exit(0);
+                    default -> System.out.println("Invalid options");
+                }
+            } while(options != 0);
         }
+    }
