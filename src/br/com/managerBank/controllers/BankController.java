0888904c5991df9b43922a935171c561bb6c3f95
Leandro Sousa Silva <135087138+Eusousleandro@users.noreply.github.com>
2026-08-22 21:01:47 -0300

    Implementando o controller

--- src/br/com/managerBank/controllers/BankController.java
+++ src/br/com/managerBank/controllers/BankController.java
@@ -2,10 +2,10 @@ package br.com.managerBank.controllers;
 
 public class BankController {
     private final static Scanner scanner = new Scanner(System.in);
-    private final static BankService bankService
+    private final static BankService service
 
     public BankController(BankService bankService) {
-        this.bankService = bankService:
+        this.bankService = service:
     }
 
     public static void deposit() {
@@ -15,14 +15,29 @@ public class BankController {
     }
 
     public static void withdraw() {
-
+        System.out.println("What the value for withdraw?");
+        double withdraw = scanner.nextDouble();
+        System.out.println("The Widthdraw of value of " + withdraw + " with sucess.");
     }
 
     public static void transferer() {
+        System.out.println("Branch number:");
+        int agency = scanner.nextInt();
+
+        System.out.println("Account number:");
+        int account = scanner.nextInt();
         
+        System.out.println("People name:");
+        String name = scanner.nextLine();
+
+        System.out.println("What's is the transferer value?");
+        double valueTransferer = scanner.nextDouble();
+
+        System.out.println("The value of transferer " + valueTransferer + " for agency" + agency + " and account " + account
+        + " People name: " + name + " carried out with sucess.");
     }
 
     public static void showBalance() {
-
+        System.out.println("The balance of value is of ")
     }
 }
