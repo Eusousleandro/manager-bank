e1f6a14afb90d00755dd73b7fdf80a4f3ad74529
Leandro Sousa Silva <135087138+Eusousleandro@users.noreply.github.com>
2026-08-23 12:43:34 -0300

    Refaturando o controller bank

--- src/br/com/managerBank/controllers/BankController.java
+++ src/br/com/managerBank/controllers/BankController.java
@@ -1,57 +1,56 @@
 package br.com.managerBank.controllers;
 
-<<<<<<< HEAD
+ 
 import java.util.Scanner;
 
 public class BankController {
 
-   private final static Scanner scanner = new Scanner(System.in);
-
-   public static void deposit() {
-       System.out.println("Qual valor você deseja depositar hoje?");
-       double value = scanner.nextDouble();
-       System.out.println("O deposito no valor de " + value + " foi realizado com sucesso.")
-   }
-=======
-public class BankController {
     private final static Scanner scanner = new Scanner(System.in);
-    private final static BankService service
-
-    public BankController(BankService bankService) {
-        this.bankService = service:
-    }
 
     public static void deposit() {
-        System.out.println("What the value to you want deposite?");
+        System.out.println("Qual valor você deseja depositar hoje?");
         double value = scanner.nextDouble();
-        System.out.println("The deposite in value " + value + " with sucess");
+        System.out.println("O deposito no valor de " + value + " foi realizado com sucesso.")
     }
+                            
+    public class BankController {
+        private final static Scanner scanner = new Scanner(System.in);
+        private final static BankService service
 
-    public static void withdraw() {
-        System.out.println("What the value for withdraw?");
-        double withdraw = scanner.nextDouble();
-        System.out.println("The Widthdraw of value of " + withdraw + " with sucess.");
+        public BankController(BankService bankService) {
+            this.bankService = service:
+        }
+
+        public static void deposit() {
+            System.out.println("What the value to you want deposite?");
+            double value = scanner.nextDouble();
+            System.out.println("The deposite in value " + value + "carried out with sucess");
+        }
+
+        public static void withdraw() {
+            System.out.println("What the value for withdraw?");
+            double withdraw = scanner.nextDouble();
+            System.out.println("The Widthdraw of value of " + withdraw + " with sucess.");
+        }
+
+        public static void transferer() {
+            System.out.println("Branch number:");
+            int agency = scanner.nextInt();
+
+            System.out.println("Account number:");
+            int account = scanner.nextInt();
+
+            System.out.println("People name:");
+            String name = scanner.nextLine();
+
+            System.out.println("What's is the transferer value?");
+            double valueTransferer = scanner.nextDouble();
+
+            System.out.println("The value of transferer " + valueTransferer + " for agency" + agency + " and account " + account
+            + " People name: " + name + " carried out with sucess.");
+        }
+
+        public static void showBalance() {
+            System.out.println("The balance of value is of ")
+        }                                                                                                                                                                                             
     }
-
-    public static void transferer() {
-        System.out.println("Branch number:");
-        int agency = scanner.nextInt();
-
-        System.out.println("Account number:");
-        int account = scanner.nextInt();
-        
-        System.out.println("People name:");
-        String name = scanner.nextLine();
-
-        System.out.println("What's is the transferer value?");
-        double valueTransferer = scanner.nextDouble();
-
-        System.out.println("The value of transferer " + valueTransferer + " for agency" + agency + " and account " + account
-        + " People name: " + name + " carried out with sucess.");
-    }
-
-    public static void showBalance() {
-        System.out.println("The balance of value is of ")
-    }
->>>>>>> 1c9b05691ba86a7b1e5ed1a12765655ba0e76e42
-}
